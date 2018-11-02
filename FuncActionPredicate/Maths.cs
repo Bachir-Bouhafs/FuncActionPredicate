d6fe9ef9522e68075938c9998a17227b58af00f1
Bachir-Bouhafs <bouhafs-bachir@outlook.fr>
2018-11-02 02:36:58 +0100

    first commit

---
+++ FuncActionPredicate/Maths.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncActionPredicate
+{
+    public class Maths
+    {
+        public delegate double delegateReturnSum(int x, float y, double z);
+        public delegate void delegateWriteSum(int x, float y, double z);
+        public delegate bool delegateCheck(string str);
+
+        public static double Somme(int x,float y , double z)
+        {
+            return x + y + z;
+        }
+
+        public static void EcrireSomme(int x, float y, double z)
+        {
+            Console.WriteLine(x + y + z);
+        }
+
+        public static bool VerifierTaille(string str)
+        {
+            if (str.Length > 4)
+                return true;
+            return false;
+        }
+
+    }
+}
---
+++ FuncActionPredicate/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncActionPredicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Maths.delegateReturnSum delegateReturn = Maths.Somme;
+            double resultat = delegateReturn.Invoke(366, 4.5f, 12.85);
+            Console.WriteLine(resultat);
+
+            Maths.delegateWriteSum delegateWrite = Maths.EcrireSomme;
+            delegateWrite.Invoke(366, 4.5f, 12.85);
+       
+
+            Maths.delegateCheck delegateCheck = Maths.VerifierTaille;
+            bool check = delegateCheck.Invoke("Bachir");
+            Console.WriteLine(check);
+
+           
+
+            //Tout le code qu'on a écrit précédemment, on peut le simplifier grâce aux delegates generic 
+            // Func, Action et Predicate
+
+            Func<int, float, double, double> objFunc = (x, y, z) =>
+            {
+                return x + y + z;
+            };
+
+            Action<int, float, double> objAction = (x, y, z) =>
+            {
+                Console.WriteLine(x + y + z);
+            };
+
+            Predicate<string> objPredicate = (str) =>
+            {
+                if (str.Length > 4)
+                    return true;
+                return false;
+            };
+
+            Console.WriteLine(objFunc.Invoke(366, 4.5f, 12.85));
+            objAction.Invoke(366, 4.5f, 12.85);
+            Console.WriteLine(objPredicate("Bachir"));
+
+            Console.ReadLine();
+
+        }
+    }
+}
